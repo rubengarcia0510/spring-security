f268b3ef6a67fe2a872b2db93a7df072f697c563
Ruben <rubengarcia0510@gmail.com>
2022-10-25 23:26:10 -0300

    add form base authentication

--- src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -33,7 +33,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/orders").hasRole("ADMIN")
 			.anyRequest().authenticated()
 			.and()
-			.httpBasic();
+			.formLogin()
+			.loginPage("/login")
+			.failureUrl("/login?error")
+			.permitAll()
+			.and()
+			.logout()
+			.clearAuthentication(true)
+			.invalidateHttpSession(true)
+			.logoutSuccessUrl("/login?logout")
+			.permitAll();
 	}
 	
 	@Bean
